d7030b6284f6fb1f2a36e137080996ead94ef73e
Sunil <sv784333@gmial.com>
2024-03-22 20:18:00 +0530

    sign up ui design

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { FaMoon,FaSun } from "react-icons/fa";
 export default function Header() {
   const path = useLocation().pathname;
   return (
-    <Navbar className='border-b-2 w-screen h-screen px-10'>
+    <Navbar className='border-b-2 w-screen  px-10'>
   <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
     <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
       Sunil's Blog
@@ -32,7 +32,7 @@ export default function Header() {
     </Button>
     <Link to='/sign-in'>
     <button class=" w-13 h-10  relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-<span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 Sign-in
 </span>
 </button>
--- my-project/src/pages/Signup.jsx
+++ my-project/src/pages/Signup.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-
+import {Label ,TextInput ,Button } from "flowbite-react"
+import { Link } from 'react-router-dom'
 function Signup() {
   return (
-    <div>Signup</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-5  flex-col md:flex-row md:items-center'>
+         <div className='flex-1'>
+         <Link to='/' className=' text-5xl text-black   font-bold dark:text-white'>
+    <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+      Sunil's
+    </span>Blogs
+  </Link>
+  <p className='m-4 text-teal-800 text-xl italic'>To see the my blogs Please sign up using email or a Google account</p>
+         </div>
+         <div className='flex-1'>
+          <form className='flex flex-col gap-4' >
+           <Label >Your Username</Label>
+           <TextInput 
+              type='text'
+              placeholder="Username"
+              id='userName'
+            />
+            <Label >Your Email</Label>
+           <TextInput 
+              type='text'
+              placeholder="name@company.com"
+              id='email'
+            />
+            <Label >Your Password</Label>
+           <TextInput 
+              type='text'
+              placeholder="Password"
+              id='password'
+            />
+             <Button outline  gradientDuoTone="purpleToPink" >
+        Sign up
+      </Button>
+          </form>
+         </div>
+      </div>
+    </div>
   )
 }
 
