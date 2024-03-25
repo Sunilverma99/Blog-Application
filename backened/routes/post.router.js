b3998317be4a0e0dc43707334d770a04805e3c19
Sunil <sv784333@gmial.com>
2024-03-26 00:00:30 +0530

    adding the getpost

--- backened/routes/post.router.js
+++ backened/routes/post.router.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { Router } from "express";
-import {createPost} from "../controller/post.controller.js"
+import {createPost,getposts} from "../controller/post.controller.js"
 import { verifyToken } from "../utlies/userverify.js";
 const router=express.Router();
 router.post("/post/create", verifyToken ,createPost);
+router.get("/posts",getposts);
 export default router;
