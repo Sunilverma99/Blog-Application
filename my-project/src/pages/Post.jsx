8452b7b6b63a136e86e926b62ed014ee043885a7
Sunil <sv784333@gmial.com>
2024-03-27 12:46:52 +0530

    Styling the post page

--- my-project/src/pages/Post.jsx
+++ my-project/src/pages/Post.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-import { Spinner } from 'flowbite-react';
+import { useParams,Link } from 'react-router-dom';
+
+import { Spinner,Button } from 'flowbite-react';
 export default function Post() {
     const[loading,setLoading]=useState(false);
     const[post,setPost]=useState(null);
@@ -35,48 +36,46 @@ export default function Post() {
       </div>
     );
     return (
-        <div>Post</div>
-        // <main 
-        //className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
-        //   <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
-        //     {post && post.title}
-        //   </h1>
-        //   <Link
-        //     to={`/search?category=${post && post.category}`}
-        //     className='self-center mt-5'
-        //   >
-        //     <Button color='gray' pill size='xs'>
-        //       {post && post.category}
-        //     </Button>
-        //   </Link>
-        //   <img
-        //     src={post && post.image}
-        //     alt={post && post.title}
-        //     className='mt-10 p-3 max-h-[600px] w-full object-cover'
-        //   />
-        //   <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
-        //     <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        //     <span className='italic'>
-        //       {post && (post.content.length / 1000).toFixed(0)} mins read
-        //     </span>
-        //   </div>
-        //   <div
-        //     className='p-3 max-w-2xl mx-auto w-full post-content'
-        //     dangerouslySetInnerHTML={{ __html: post && post.content }}
-        //   ></div>
-        //   <div className='max-w-4xl mx-auto w-full'>
-        //     <CallToAction />
-        //   </div>
-        //   <CommentSection postId={post._id} />
+      
+        <main 
+        className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
+          <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
+            {post && post.title}
+          </h1>
+          <Link
+            to={`/search?category=${post && post.category}`}
+            className='self-center mt-5'
+          >
+            <Button color='gray' pill size='xs'>
+              {post && post.category}
+            </Button>
+          </Link>
+          <img
+            src={post && post.photoUrl}
+            alt={post && post.title}
+            className='mt-10 p-3 max-h-[600px] w-full object-cover'
+          />
+          <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
+            <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+            <span className='italic'>
+              {post && (post.content.length / 1000).toFixed(0)} mins read
+            </span>
+          </div>
+          <div
+            className='p-3 max-w-2xl mx-auto w-full post-content'
+            dangerouslySetInnerHTML={{ __html: post && post.content }}
+          ></div>
+         
+
     
-        //   <div className='flex flex-col justify-center items-center mb-5'>
-        //     <h1 className='text-xl mt-5'>Recent articles</h1>
-        //     <div className='flex flex-wrap gap-5 mt-5 justify-center'>
-        //       {recentPosts &&
-        //         recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
-        //     </div>
-        //   </div>
-        // </main>
+          {/* <div className='flex flex-col justify-center items-center mb-5'>
+            <h1 className='text-xl mt-5'>Recent articles</h1>
+            <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+              {recentPosts &&
+                recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+            </div>
+          </div> */}
+        </main>
     );
 
 }
